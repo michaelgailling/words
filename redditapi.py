80a54898972cfce984e9325ee2d775cd0a17f751
michaelgailling <michael.gailling@gmail.com>
2017-05-13 13:54:18 -0400

    Initial commit

---
+++ redditapi.py
@@ -0,0 +1,30 @@
+import urllib.request
+from html.parser import HTMLParser
+
+
+from html.parser import HTMLParser
+
+class ParseContent(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+parser = ParseContent()
+
+
+
+
+url = ""
+hdr = { 'User-Agent' : 'metaprinter' }
+req = urllib.request.Request(url, headers=hdr)
+html = urllib.request.urlopen(req).read()
+
+html = html.decode("utf-8")
+
+
+parser.feed(html)
