30403c06ade5898520163e89daf13ced755793e3
michaelgailling <michael.gailling@gmail.com>
2017-05-13 14:10:43 -0400

    Small Fix

--- redditapi.py
+++ redditapi.py
@@ -6,7 +6,7 @@ parser = ParseContent()
 
 usr = ""
 postnum = 0
-url = "https://www.reddit.com/user/"+usr+"/"+str(postnum)
+url = "https://www.reddit.com/user/"+usr+"/?count="+str(postnum)
 hdr = { 'User-Agent' : 'metaprinter' }
 req = urllib.request.Request(url, headers=hdr)
 html = urllib.request.urlopen(req).read()
