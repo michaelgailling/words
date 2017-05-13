8508fa2b8c307831a783bf25bb1d457c7130976e
michaelgailling <michael.gailling@gmail.com>
2017-05-13 14:06:32 -0400

    Splitting up files for better organization

--- redditapi.py
+++ redditapi.py
@@ -1,23 +1,8 @@
 import urllib.request
-from html.parser import HTMLParser
-
-#Override and redefine methods from the html parser class
-class ParseContent(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
+from Parser import ParseContent
 
 #Crreate new handler for the parser class
 parser = ParseContent()
-
-
-
-
 
 usr = ""
 postnum = 0
