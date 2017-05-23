93440f7b9fd8a389848bc164987e649c4ad1fc5b
michaelgailling <michael.gailling@gmail.com>
2017-05-23 01:16:15 -0400

    Removed unnecessary variables

--- HtmlScrape.py
+++ HtmlScrape.py
@@ -4,9 +4,8 @@ from SourceParser import ParseContent
 #Crreate new handler for the parser class
 parser = ParseContent()
 
-usr = ""
-postnum = 0
-url = "https://www.reddit.com/user/"+usr+"/?count="+str(postnum)
+
+url = ""
 hdr = { 'User-Agent' : 'metaprinter' }
 req = urllib.request.Request(url, headers=hdr)
 html = urllib.request.urlopen(req).read()
